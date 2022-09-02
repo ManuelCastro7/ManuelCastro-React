a9bf85f388b76b8b3924a1dfe542acfce79b38ca
Manuel Castro <manuel.castro.ofc@gmail.com>
2022-09-02 15:03:56 -0300

    Segundo desafio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>CompraGamer</h1>
+        <div className="container">
+          <button>Opcion 1</button>
+          <button>Opcion 2</button>
+          <button>Opcion 3</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
