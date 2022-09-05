3970a9451ed3c337a62510cde9f5ed3f321969cc
Manuel Castro <manuel.castro.ofc@gmail.com>
2022-09-05 16:59:40 -0300

    Desafio 3

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,15 @@ import CartWidget from "./CartWidget";
 
 function NavBar(){
     return(
-      <div>
         <header>
           <h1>GAMER-SHOP</h1>
-          <div className="container">
+          <div>
             <a href="">Enlace1</a>
             <a href="">Enlace2</a>
             <a href="">Enlace3</a>
           </div>
           <CartWidget />
         </header>
-      </div>
     );
   }
 
