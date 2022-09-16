cd191f9a643c93a0f19d039a06be31220e3914f1
Manuel Castro <manuel.castro.ofc@gmail.com>
2022-09-16 13:08:45 -0300

    Desafio 5

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,7 @@ import Count from "./ItemCount";
 const Item = ({product}) => {
     return(
         <div className="card">
-            <image src="./img/fondo.jpg" alt="product image" />
+            <image>{product.image}</image>
             <h2>{product.name}</h2>
             <h2>{product.price}</h2>
             <h2>Stock : {product.stock}</h2>
