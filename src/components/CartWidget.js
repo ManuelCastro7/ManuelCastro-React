b6aa8363b135eacd3ee3776f39cd6d5f9b0ba1a5
Manuel Castro <manuel.castro.ofc@gmail.com>
2022-09-26 23:54:30 -0300

    Desafio 10

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,7 +1,18 @@
-function CartWidget(){
-    return(
-        <img src="https://img.icons8.com/ios-glyphs/42.5/00000/shopping-cart--v1.png" alt="Cart"/>
-    )
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { cartContext } from '../context/CartContextProvider';
+
+const CartWidget = () => {
+
+  const { totalProductsQty } = useContext(cartContext);
+
+  return (
+    <>
+      <div>
+      <Link to="/cart"><img src="https://img.icons8.com/ios-glyphs/42.5/00000/shopping-cart--v1.png" alt="Cart"/></Link> {(totalProductsQty() !== 0) && <span className='badge'>{totalProductsQty()}</span>}
+      </div>
+    </>
+  )
 }
 
 export default CartWidget;
